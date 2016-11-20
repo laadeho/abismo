da45769f5cb9e09bb98d483d07ca9c42b910cdcc
laadeho <eduardobieta@gmail.com>
2016-11-20 14:07:30 -0600

    Actualiza archivos test de audio y video

---
+++ grafica/v_test01.js
@@ -0,0 +1,15 @@
+function setup(){
+	createCanvas(windowWidth, windowHeight);
+
+}
+function draw(){
+	ellipse(random(width), random(height), 10, 10)
+
+}
+
+function reset(){
+	background(255);
+}
+function mousePressed(){
+	background(255);
+}
