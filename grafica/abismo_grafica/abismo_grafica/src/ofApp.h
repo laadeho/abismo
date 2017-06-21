12d6bd9155ca42cbfaeefb26e8fe7a0776f37d8b
laadeho <eduardobieta@gmail.com>
2017-06-21 16:43:22 -0500

    web update

--- grafica/abismo_grafica/abismo_grafica/src/ofApp.h
+++ grafica/abismo_grafica/abismo_grafica/src/ofApp.h
@@ -8,8 +8,8 @@
 
 #define PORT 7000
 #define NUM_MSG_STRINGS 20
-#define puertoCOM1 "COM6"
-#define puertoCOM2 "COM7"
+#define puertoCOM1 "COM11"
+#define puertoCOM2 "COM13"
 
 class ofApp : public ofBaseApp{
 
